30d8f36ac34d76030b4345441321f473229ed158
Naana Shifah <shifahnana2@gmail.com>
2025-08-31 17:15:35 +0300

    feat: update app/index.tsx with entry screen text and styled components

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -3,14 +3,9 @@ import { StyleSheet, View, Text } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.mainText}>Entry Screen - Awesome</Text>
-
-      <Text style={styles.largeText}>
-        Typescript is great if you practice more
-      </Text>
-      <Text style={styles.mediumText}>
-        React Native provides you a single codebase for cross platforms
-      </Text>
+      <Text>Entry Screen - Awesome</Text>
+      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
       <Text style={styles.smallText}>ALX is awesome</Text>
     </View>
   );
@@ -22,22 +17,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
-    padding: 20,
-  },
-  mainText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 20,
   },
   largeText: {
-    fontSize: 18,
+    fontSize: 20,
     color: "blue",
-    marginBottom: 10,
   },
   mediumText: {
     fontSize: 16,
     color: "green",
-    marginBottom: 10,
   },
   smallText: {
     fontSize: 14,
